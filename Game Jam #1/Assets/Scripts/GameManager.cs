6992e998f5e0615cf2c645350ae422c2d6177111
Nicholas Choi <Nicholas.Choi@amd.com>
2020-09-19 15:07:14 -0400

    Added maze and audio and win condition

---
+++ Game Jam #1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < -15)
+        {
+            this.Restart();
+        }
+    }
+
+    public void WinGame()
+    {
+        this.Restart();
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
